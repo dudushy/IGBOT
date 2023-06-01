a3529ca47e5a354cbb7e80a7711b598a26b90bf8
dudushy <eduardotalarico1337@gmail.com>
2023-06-01 20:50:26 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,32 @@
 # pip freeze | xargs pip uninstall -y
 # pip freeze > requirements.txt
-#* -- Imports
+# * -- Imports
 import os
+import sys
+from dotenv import load_dotenv
 
-#* -- Variables
-path = os.path.dirname(__file__) #? Directory path
+# * -- Variables
+title = "IGBOT"  # ? Title
+path = os.path.dirname(__file__)  # ? Directory path
 
-#* -- Functions
-def clearConsole() -> None: #? Clear console
+load_dotenv(dotenv_path=".env")  # ? Load .env file
+test = os.getenv("TEST")  # ? Test variable
+
+# * -- Functions
+
+
+def clearConsole() -> None:  # ? Clear console
     os.system("cls" if os.name == "nt" else "clear")
+
 
 def main() -> None:
     pass
+
 
 #! Main
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        print(e)
+        _, _, exc_tb = sys.exc_info()
+        print(f"[{title}#__main__] error (line: {exc_tb.tb_lineno}): ", e)
