55fa7244b1e6b08a4b96e786cff10d16b3c06a47
dudushy <eduardotalarico1337@gmail.com>
2023-06-01 21:53:19 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,14 +3,31 @@
 # * -- Imports
 import os
 import sys
+
 from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 # * -- Variables
 title = "IGBOT"  # ? Title
-path = os.path.dirname(__file__)  # ? Directory path
+PATH = os.path.realpath(__file__)  # ? Directory path
 
 load_dotenv(dotenv_path=".env")  # ? Load .env file
-test = os.getenv("TEST")  # ? Test variable
+TEST = os.getenv("TEST")  # ? Test variable
+
+USER_DATA_DIR = f"{PATH}\\data"
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument(f"--headless")
+# chrome_options.add_argument(f"--user-data-dir={USER_DATA_DIR}")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+driver = webdriver.Chrome(service=Service(
+    ChromeDriverManager().install()), options=chrome_options)
+
 
 # * -- Functions
 
@@ -19,8 +36,25 @@ def clearConsole() -> None:  # ? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
 
+def test() -> None:
+    print(f"[{title}#test] PATH", PATH)
+    print(f"[{title}#test] test", TEST)
+    print(f"[{title}#test] USER_DATA_DIR", USER_DATA_DIR)
+
+
 def main() -> None:
-    print(f"[{title}#main] test", test)
+    test()
+
+    driver.get("https://www.instagram.com/")
+    # elem = driver.find_element(By.NAME, "q")
+    # elem.clear()
+    # elem.send_keys("pycon")
+    # elem.send_keys(Keys.RETURN)
+    # driver.send_keys(Keys.RETURN)
+
+    input(f"[{title}#main] Press Enter to close...")
+
+    driver.close()
 
 
 #! Main
