cd1b133c5f913f940a2aa2909879576c43690ea5
dudushy <eduardotalarico1337@gmail.com>
2023-06-10 18:15:23 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -34,7 +34,6 @@ chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 DRIVER = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=chrome_options)
-DRIVER_WAIT = WebDriverWait(DRIVER, 20)
 
 
 # * -- Functions
@@ -53,6 +52,25 @@ def checkVars() -> None:
     print(f"[{title}#checkVars] PASSWORD", PASSWORD)
 
 
+def wait4element(xpath, mode="presence", delay=10) -> bool:
+    print(f"[{title}#wait4element] (BEFORE) mode={mode} delay={delay}")
+    try:
+        if (mode == "presence"):
+            WebDriverWait(DRIVER, delay).until(EC.presence_of_element_located(
+                (By.XPATH, xpath))
+            )
+            print(f"[{title}#wait4element] (AFTER) {True}")
+            return True
+        if (mode == "clickable"):
+            WebDriverWait(DRIVER, delay).until(EC.element_to_be_clickable(
+                (By.XPATH, xpath))
+            )
+            print(f"[{title}#wait4element] (AFTER) {True}")
+            return True
+    except Exception as e:
+        print(f"[{title}#wait4element] (AFTER) {False} | error: ", e)
+        return False
+
 def main() -> None:
     try:
         # clearConsole()
@@ -64,20 +82,18 @@ def main() -> None:
 
         # ? Insert username and password
         print(f"[{title}#main] searching username input...")
-        element = DRIVER_WAIT.until(EC.presence_of_element_located(
-            (By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'))
-        )
-        print(f"[{title}#main] found username input")
-        element.send_keys(USERNAME)
-        print(f"[{title}#main] typed username")
+        if (wait4element('//*[@id="loginForm"]/div/div[1]/div/label/input')):
+            print(f"[{title}#main] found username input")
+            element = DRIVER.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+            element.send_keys(USERNAME)
+            print(f"[{title}#main] typed username")
 
         print(f"[{title}#main] searching password input...")
-        element = DRIVER_WAIT.until(EC.presence_of_element_located(
-            (By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input'))
-        )
-        print(f"[{title}#main] found password input")
-        element.send_keys(PASSWORD)
-        print(f"[{title}#main] typed password")
+        if (wait4element('//*[@id="loginForm"]/div/div[2]/div/label/input')):
+            print(f"[{title}#main] found password input")
+            element = DRIVER.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+            element.send_keys(PASSWORD)
+            print(f"[{title}#main] typed password")
 
         # ? Login and wait
         element.send_keys(Keys.ENTER)
@@ -85,31 +101,28 @@ def main() -> None:
         time.sleep(5)
 
         # ? Dissmiss save login info
-        # print(f"[{title}#main] searching info not now button...")
-        # element = DRIVER_WAIT.until(EC.element_to_be_clickable(
-        #     (By.XPATH, '//*[@id="mount_0_0_mV"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div'))
-        # )
-        # print(f"[{title}#main] found info not now button")
-        # element.click()
-        # print(f"[{title}#main] clicked info not now button")
+        print(f"[{title}#main] searching info not now button...")
+        if (wait4element('//*[@id="mount_0_0_mV"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div', mode="clickable")):
+            print(f"[{title}#main] found info not now button")
+            element = DRIVER.find_element(By.XPATH, '//*[@id="mount_0_0_mV"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div')
+            element.click()
+            print(f"[{title}#main] clicked info not now button")
 
         # ? Dissmiss notifications
-        # print(f"[{title}#main] searching notifications not now button...")
-        # element = DRIVER_WAIT.until(EC.element_to_be_clickable(
-        #     (By.XPATH, '//*[@id="mount_0_0_1a"]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]'))
-        # )
-        # print(f"[{title}#main] found notifications not now button")
-        # element.send_keys(Keys.ENTER)
-        # print(f"[{title}#main] clicked notifications not now button")
+        print(f"[{title}#main] searching notifications not now button...")
+        if (wait4element('//*[@id="mount_0_0_1a"]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]', mode="clickable")):
+            print(f"[{title}#main] found notifications not now button")
+            element = DRIVER.find_element(By.XPATH, '//*[@id="mount_0_0_1a"]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
+            element.click()
+            print(f"[{title}#main] clicked notifications not now button")
 
         # ? Click create button
         print(f"[{title}#main] searching create button...")
-        element = DRIVER_WAIT.until(EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="mount_0_0_hA"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[7]/div/span/div/a/div/div[1]/div/div/svg'))
-        )
-        print(f"[{title}#main] found create button")
-        element.click()
-        print(f"[{title}#main] clicked create button")
+        if (wait4element('//*[@id="mount_0_0_hA"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[7]/div/span/div/a/div/div[1]/div/div/svg', mode="clickable")):
+            print(f"[{title}#main] found create button")
+            element = DRIVER.find_element(By.XPATH, '//*[@id="mount_0_0_hA"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[7]/div/span/div/a/div/div[1]/div/div/svg')
+            element.click()
+            print(f"[{title}#main] clicked create button")
 
         # ? Close
         input(f"[{title}#main] Press Enter to close...")
